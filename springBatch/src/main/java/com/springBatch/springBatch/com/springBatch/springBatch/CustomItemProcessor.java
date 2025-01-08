f6972e52df4a94b5335d6d9cf0eb39728cde37fa
isayantan18 <146025087+isayantan18@users.noreply.github.com>
2025-01-08 12:42:04 +0530

    Update CustomItemProcessor.java

--- springBatch/src/main/java/com/springBatch/springBatch/com/springBatch/springBatch/CustomItemProcessor.java
+++ springBatch/src/main/java/com/springBatch/springBatch/com/springBatch/springBatch/CustomItemProcessor.java
@@ -9,16 +9,7 @@ public class CustomItemProcessor implements ItemProcessor<Products, Products> {
     @Override
     public Products process(Products item) throws Exception {
 
-		try {/*
-				 * 
-				 * // put the percentage logic System.out.println(item.getDescription()); int
-				 * int discountPer = Integer.parseInt(item.getDiscount().trim()); double
-				 * originalPrice = Double.parseDouble(item.getPrice().trim()); double discount =
-				 * (discountPer / 100) * originalPrice; double finalPrice = originalPrice -
-				 * discount;
-				 */
-				 
-            //item.setDiscountedPrice(String.valueOf(finalPrice));
+		try {
         } catch (
                 NumberFormatException ex
         ) {
